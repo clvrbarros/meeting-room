9db248d6d5489023d8002cfd774131ef8563bdd2
Cleverson Barros <cleversonferreirab@gmail.com>
2020-11-20 17:35:26 -0300

    add exceptions handler

---
+++ backend/src/main/java/com/example/meetingroom/exception/ErrorDetails.java
@@ -0,0 +1,17 @@
+package com.example.meetingroom.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ErrorDetails {
+
+    private Date timestamp;
+    private String message;
+    private String details;
+}
